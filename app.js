649b624fdc07e7f41af778d9b934f30ac2bd5d29
Miles Angels <milesmarieangels@gmail.com>
2023-11-29 16:43:30 -0800

    added function to get items from storage, add items to storage, display items

--- app.js
+++ app.js
@@ -16,8 +16,15 @@ const itemFilter = document.getElementById('filter');
 const itemClear = document.getElementById('clear-display');
 
 /* Main Functions */
+
+let displayItems = () => {
+    const itemsFromStorage = getItemsFromStorage();
+    itemsFromStorage.forEach(item => addItemToDOM(item));
+    checkUI();
+}
+
 // Add items function
-let addItem = (event) => {
+let onAddItemSubmit = (event) => {
     event.preventDefault();
 
     // We can only get the accurate value after the user hits the add button
@@ -29,19 +36,11 @@ let addItem = (event) => {
         return;
     }
 
-    // Create list item and add class/es needed
-    const li = document.createElement('li');
-    li.classList.add('item');
+    // Create item to DOM
+    addItemToDOM(newItem);
 
-    // Append the value from newItem to list item
-    li.appendChild(document.createTextNode(newItem));
-
-    // Create removeButton via the createButton function pass the class/es needed and append it to the list item
-    const removeButton = createButton('remove-item-btn');
-    li.appendChild(removeButton);
-
-    // Append the list item to the ul called itemList
-    itemList.appendChild(li);
+    // Add item to local storage
+    addItemToStorage(newItem);
 
     // Check if there are items are being displayed.
     checkUI();
@@ -50,13 +49,55 @@ let addItem = (event) => {
     form.reset();
 }
 
+let addItemToDOM = (item) => {
+    // Create list item and add class/es needed
+    const li = document.createElement('li');
+    li.classList.add('item');
+
+    // Append the value from newItem to list item
+    li.appendChild(document.createTextNode(item));
+
+    // Create removeButton via the createButton function pass the class/es needed and append it to the list item
+    const removeButton = createButton('remove-item-btn');
+    li.appendChild(removeButton);
+
+    // Append the list item to the ul called itemList
+    itemList.appendChild(li);
+}
+
+let addItemToStorage = (item) => {
+    const itemsFromStorage = getItemsFromStorage();
+
+    // Add new item to array
+    itemsFromStorage.push(item);
+
+    // Convert to JSON string and set to local storage
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
+let getItemsFromStorage = () => {
+    let itemsFromStorage;
+
+    // Check if local storage is empty
+    if(localStorage.getItem('items') === null) {
+        // If local storage is empty, then set itemsFromStorage to an empty array
+        itemsFromStorage = [];
+    }
+    else {
+        // If local storage has items in it, then parse its content and get the items
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+    }
+
+    return itemsFromStorage;
+}
+
 // Remove items function
 let removeItem = (event) => {
 
     // Check if the element we are targeting has a parent element with a class called remove-item-btn
     if (event.target.parentElement.classList.contains('remove-item-btn')) {
         if (confirm('Are you sure that you want to delete the item/s?')) {
-            // If true, remove the entire list item
+            // If true, remove the entire list item and check the DOM if there are items displayed
             event.target.parentElement.parentElement.remove();
             checkUI();
         }
@@ -70,11 +111,39 @@ let clearItems = () => {
     while (itemList.firstChild) {
         itemList.removeChild(itemList.firstChild);
     }
-    
+
+    // Check the DOM to reset the state of the DOM to not display 
+    // the clear button and filter input
     checkUI();
 }
 
-// Check if there are displayed items in the DOM
+// Filter items based on user input filter
+let filterItems = (event) => {
+    // Get the user input filter and transform it to lowercase letters
+    const filterText = event.target.value.toLowerCase();
+
+    // Get each item list 
+    const displayedItems = itemList.querySelectorAll('li');
+
+    // Iterate through each item
+    displayedItems.forEach(item => {
+
+        // Get each list item's text
+        const itemName = item.firstChild.textContent.toLowerCase();
+
+        // Check if list item text and user input filter is equal to each other
+        if (itemName.indexOf(filterText) != -1) {
+            // If true, set display to flex
+            item.style.display = 'flex';
+        }
+        else {
+            // If false, set display to none
+            item.style.display = 'none';
+        }
+    });
+}
+
+// Check if there are items displayed
 let checkUI = () => {
     const displayedItems = itemList.querySelectorAll('li');
 
@@ -121,9 +190,17 @@ let createIcon = (iconClasses) => {
     return icon;
 }
 
-// Event Listers
-form.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
-itemClear.addEventListener('click', clearItems);
+// Initialize app
 
-checkUI();
+function init() {
+    // Event Listers
+    form.addEventListener('submit', onAddItemSubmit);
+    itemList.addEventListener('click', removeItem);
+    itemClear.addEventListener('click', clearItems);
+    itemFilter.addEventListener('input', filterItems);
+    document.addEventListener('DOMContentLoaded', displayItems);
+
+    checkUI();
+}
+
+init();
